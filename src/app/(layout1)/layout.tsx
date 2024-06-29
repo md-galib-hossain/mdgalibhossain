380fc8c3d6e516ae1c25fdc10d3ba0d74de21cb0
Md Galib Hossain <mdgalib23@gmail.com>
2024-06-29 11:35:34 +0600

    next js initiate & mui setup

---
+++ src/app/(layout1)/layout.tsx
@@ -0,0 +1,13 @@
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      layout
+      <div className="min-h-screen">
+
+      {children}
+      </div>
+    </>
+  );
+};
+
+export default layout;
---
+++ src/app/(layout1)/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>Md Galib hossain portfolio
+</div>
+  )
+}
+
+export default Homepage
